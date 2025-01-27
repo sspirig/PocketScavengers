96082b26fdc5b21ff6352275f56a0ff950697906
sspirig <santiago.sprg@eduge.ch>
2025-01-27 13:01:45 +0100

    Ver.1 - Added html pages and mapbox javascript integration

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * @name PocketScavengers
+ * @file main.js
+ * @description script js qui rejoint toutes les intégrations, fonctions et instances de classes  
+ * @author Santiago SPRG
+ * @version 27.01.2025 Ver 1.
+*/
